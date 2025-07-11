99caebf698cdfb9c83636b791e9757f60f8e86d2
Chaizee <zuzindima26@gmail.com>
2025-07-11 22:48:57 +0600

    feat: add dinamic libraries realisation

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Reflection;
+using CommandLib;
+
+static void Main(string[] args) {
+
+    string path = Path.Combain($"{Directory.GetCurrentDirectory()}", "TestDir");
+
+    Assambly assambly = Assambly.LoadFrom("../FileSystemCommands/bin/Debug/net8.0/FileSystemCommands.dll"); 
+
+    var DirectSizeType = assambly.GetType("FileSystemComands.DirectorySizeCommand");
+    var DirectSizeInstance = (ICommand)Activator.CreateInstance(DirectSizeTypeype, path);
+    DirectSizeInstance.Execute();
+
+    var FindFilesType = assambly.GetType("FileSystemComands.FindFilesCommand");
+    var FindFilesInstance = (ICommand)Activator.CreateInstance(FindFilesType, path);
+    FindFilesInstance.Execute();
+
+}
---
+++ task08tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+using FileSystemCommands;
+using Xunit;
+
+namespace task08tests;
+
+public class FileSystemCommandsTests
+{
+    [Fact]
+    public void DirectorySizeCommand_ShouldCalculateSize()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+
+        var command = new DirectorySizeCommand(testDir);
+        command.Execute();
+
+        Assert.Equal(10, command.Size);
+
+        Directory.Delete(testDir, true);
+    }
+
+    [Fact]
+    public void FindFilesCommand_ShouldFindMatchingFiles()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+        File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+
+        var command = new FindFilesCommand(testDir, "*.txt");
+        command.Execute();
+
+        Assert.Equal(1, command.foundFiles.Length);
+
+        Directory.Delete(testDir, true);
+    }
+}
