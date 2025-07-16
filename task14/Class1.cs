ddd1347e26a3d194481cb14cd4c2d82f14fe098a
Chaizee <zuzindima26@gmail.com>
2025-07-16 21:50:03 +0600

    feat: add integral solve realisation

---
+++ task14/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+    {
+        double result = 0.0;
+        double stepSize = (b - a) / threadsnumber;
+        Object lockObj = new Object();
+
+        using Barrier barrier = new Barrier(threadsnumber + 1);
+
+        Thread[] threads = new Thread[threadsnumber];
+
+        for (int i = 0; i < threadsnumber; i++) {
+
+            int threadId = i;
+
+            new Thread(() => 
+            {
+                double localStart = a + threadId * stepSize;
+                double localEnd = (threadId == threadsnumber - 1) ? b : localStart + stepSize;
+
+                double _res = 0.0;
+
+                for (double j = localStart; j < localEnd; j+=step ) {
+                    
+                    double Next = Math.Min(j+step, localEnd);
+                    _res += 0.5 * (function(j) + function(Next)) * (Next - j);
+                }
+
+                lock (lockObj) { result += _res; }
+
+                barrier.SignalAndWait();
+            }).Start();
+        }
+
+        barrier.SignalAndWait();
+
+        return result;
+    }
+}
---
+++ task14tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using task14;
+
+namespace task14tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CalculatesTheLinearFunctionCorrectly()
+    {
+        var X = (double x) => x;
+        double result = DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2);
+        Assert.Equal(0, result, 1e-4);
+    }
+
+    [Fact]
+    public void CalculatesTheSinFunctionCorrectly()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        double result = DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8);
+        Assert.Equal(0, result, 1e-4);
+    }
+
+    [Fact]
+    public void CalculatesTheLinearFunctionCorrectly_ZeroToFive()
+    {
+        var X = (double x) => x;
+        double result = DefiniteIntegral.Solve(0, 5, X, 1e-6, 8);
+        Assert.Equal(12.5, result, 1e-5);
+    }
+}
